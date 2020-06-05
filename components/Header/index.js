96a68596e353e47863590ec6077da33b3e1aff55
numbers0580 <koreankid18@yahoo.com>
2020-06-05 09:35:56 -0600

    Completed header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,25 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let mainDiv = document.createElement('div');
+    let dateTag = document.createElement('span');;
+    let titleTag = document.createElement('h1');
+    let tempTag = document.createElement('span');
+
+    mainDiv.classList.add('header');
+    dateTag.classList.add('date');
+    dateTag.textContent = 'March 28, 2019';
+    titleTag.textContent = 'Lambda Times';
+    tempTag.classList.add('temp');
+    tempTag.textContent = '98°';
+
+    mainDiv.appendChild(dateTag);
+    mainDiv.appendChild(titleTag);
+    mainDiv.appendChild(tempTag);
+
+    return mainDiv;
+}
+
+let createdHeader = Header();
+document.querySelector('.header-container').appendChild(createdHeader);
